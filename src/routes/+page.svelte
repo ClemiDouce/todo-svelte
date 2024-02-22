ad694dbb4b77b22db708c7885d37a4ec059ea513
Clementine <dupycournau@gmail.com>
2024-02-22 11:26:20 +0100

    add input to create task

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  interface Todo {
+    title: string;
+    done: boolean;
+  }
+  let todoList: Todo[] = [];
+  let titleCreate = "";
+
+  function createTodo() {
+    let newTodo: Todo = { title: titleCreate, done: false };
+    todoList = [...todoList, newTodo];
+    titleCreate = "";
+    console.log(todoList);
+  }
+</script>
+
+<h1>Welcome to Todo !</h1>
+
+<h2>Todo Creation</h2>
+<input
+  type="text"
+  name="title"
+  placeholder="Todo title"
+  bind:value={titleCreate}
+/>
+<button on:click={createTodo}>Create</button>
